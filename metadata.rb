121dd9c4a880310bfe2b1c341c4e444a33633785
Bez Hermoso <bez@activelamp.com>
2014-09-12 16:32:40 -0700

    Versioning and license.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'symfony_project'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
+maintainer       'Bezalel Hermoso, ActiveLAMP'
+maintainer_email 'bez@activelamp.com'
+license          'Apache 2.0'
 description      'Installs/Configures symfony_project'
 long_description 'Installs/Configures symfony_project'
-version          '0.1.0'
+version          '0.0.1'
 
 depends 'git', '~> 4.0.2'
