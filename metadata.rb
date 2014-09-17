13b67502718faf26d0125f19ff0dbe418ff8b4ad
Bez Hermoso <bezhermoso@users.noreply.github.com>
2014-09-16 18:13:07 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'bez@activelamp.com'
 license          'Apache 2.0'
 description      'Installs/Configures symfony_project'
 long_description 'Installs/Configures symfony_project'
-version          '0.0.1'
+version          '0.0.2'
 
 depends 'git', '~> 4.0.2'
