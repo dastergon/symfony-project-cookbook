b5449b5ca32c363b8200f78879ffec2fac1abe9b
Pavlos Ratis <dastergon@gmail.com>
2015-04-08 17:00:49 +0300

    change dependency version in composer

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Cookbook for deploying Symfony2 projects.'
 long_description 'Cookbook for deploying Symfony2 projects.'
 version          '0.0.2'
 
-depends 'activelamp_composer', '~>0.0.1'
+depends 'activelamp_composer', '= 0.0.1'
 
 provides 'symfony_project[/path/to/deploy]'
 provides 'app_console[/path/to/deploy/current]'
